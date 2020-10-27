bf7cd4f2a04cd469e8643f5a1a14b4e9f7ddeb83
IdanGib <idangibly.dev@gmail.com>
2020-10-27 09:56:58 +0200

    init smart

--- docs/index.js
+++ docs/index.js
@@ -1,19 +1,32 @@
-function main() {
-    console.log('main');
-}
+(function(bootstrap) {
+    async function init() {
 
-function bootstrap() {
+        return {
+    
+        };
+    }
+    
+    async function run(settings) {
+    
+    }
+    
+    async function main() {
+        const settings = await init();
+        await run(settings);
+    }
+    
+    bootstrap(main);
+
+})(function(main) {
     const timeout = 10000;
     let dt = 0;
     const start = Date.now();
     const interval = setInterval(() => {
         dt = Date.now() - start;
-        console.log('check', dt);
         if(document.readyState === 'complete' || (timeout < dt) ) {
             clearInterval(interval);
             main();
         }
     }, 100);
-}
+});
 
-bootstrap();
