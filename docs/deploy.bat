0bad41d66dc090ea8bd8ff097045088c824dfb5d
IdanGib <idangibly.dev@gmail.com>
2020-10-28 14:30:48 +0200

    deploy

---
+++ docs/deploy.bat
@@ -0,0 +1,9 @@
+git add . 
+echo "======================"
+git commit -m "deploy"
+echo "======================"
+git push origin main
+echo "======================"
+echo "====== Done =========="
+echo "======================"
+wait
